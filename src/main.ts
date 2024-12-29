1e6dedf5025e4954c55a01eac1dfba0b7599b092
Marina <marinaoliveira.r07@gmail.com>
2024-12-29 19:50:51 -0300

    chore: project setup

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { configure } from '@codegenie/serverless-express';
+
+let server: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressHandler = app.getHttpAdapter().getInstance();
+
+  return configure({ app: expressHandler });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
